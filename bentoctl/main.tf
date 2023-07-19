7ce3c3fa537ec938996ac2821dbe2828f2ac15c7
Ubuntu <ubuntu@ip-172-31-46-121.ap-southeast-1.compute.internal>
2023-07-19 09:47:19 +0000

    first commit

---
+++ bentoctl/main.tf
@@ -0,0 +1,184 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.region
+}
+
+################################################################################
+# Input variable definitions
+################################################################################
+
+variable "deployment_name" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "ami_id" {
+  type = string
+}
+
+variable "enable_gpus" {
+  type = bool
+}
+
+variable "image_repository" {
+  type = string
+}
+
+variable "image_version" {
+  type = string
+}
+
+variable "image_tag" {
+  type = string
+}
+
+################################################################################
+# Resource definitions
+################################################################################
+
+resource "aws_iam_role" "ec2_role" {
+  name = "${var.deployment_name}-iam"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      }
+    ]
+  })
+  inline_policy {
+    name = "my_inline_policy"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action = [
+            "ecr:GetAuthorizationToken",
+            "ecr:BatchGetImage",
+            "ecr:GetDownloadUrlForLayer"
+          ]
+          Effect   = "Allow"
+          Resource = "*"
+        },
+      ]
+    })
+  }
+}
+
+resource "aws_iam_instance_profile" "ip" {
+  name = "${var.deployment_name}-instance-profile"
+  role = aws_iam_role.ec2_role.name
+}
+
+
+resource "aws_security_group" "allow_" {
+  name        = "${var.deployment_name}-sg"
+  description = "SG for  server"
+
+  ingress {
+    description      = "HTTP for "
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  # Uncomment if you want to ennable ssh access into the instance. Not for prod.
+  ingress {
+    description      = "ssh access (incase, for debugging)"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+resource "aws_launch_template" "lt" {
+  name                   = "${var.deployment_name}-lt"
+  image_id               = var.ami_id
+  instance_type          = var.instance_type
+  update_default_version = true
+  user_data              = filebase64("startup_script.sh")
+  security_group_names   = [aws_security_group.allow_.name]
+  key_name              = {var.aws_ec2_key_name}
+
+  iam_instance_profile {
+    arn = aws_iam_instance_profile.ip.arn
+  }
+  block_device_mappings {
+    device_name = "/dev/sda1"
+
+    ebs {
+      volume_size = 600
+    }
+  }
+}
+
+resource "aws_instance" "app_server" {
+  launch_template {
+    id = aws_launch_template.lt.id
+  }
+
+  provisioner "local-exec" {
+    command = <<-EOT
+        attempt_counter=0
+        max_attempts=40
+        printf 'waiting for server to start'
+        until $(curl --output /dev/null --silent --head --fail http://${self.public_ip}); do
+            if [ $attempt_counter -eq $max_attempts ];then
+              echo "Max attempts reached"
+              exit 1
+            fi
+
+            printf '.'
+            attempt_counter=$(($attempt_counter+1))
+            sleep 15
+        done
+        EOT
+  }
+}
+
+################################################################################
+# Output value definitions
+################################################################################
+output "endpoint" {
+  description = "address of ec2 instance created. You can go to that URL to interate with the service"
+  value       = "http://${aws_instance.app_server.public_ip}"
+}
+
+output "ec2_instance_status" {
+  description = "Status of the created instance"
+  value       = aws_instance.app_server.instance_state
+}
---
+++ fp32/service.py
@@ -0,0 +1,186 @@
+from hashlib import sha256
+from instance import Instance
+
+from contextlib import ExitStack
+from starlette.middleware.cors import CORSMiddleware
+
+import torch
+from torch import autocast
+from diffusers import StableDiffusionPipeline, DiffusionPipeline, StableDiffusionXLPipeline, StableDiffusionXLImg2ImgPipeline
+from diffusers import StableDiffusionImg2ImgPipeline
+from diffusers import StableDiffusionInpaintPipeline
+from huggingface_hub import login
+login(Instance.HUGGING_FACE_PWD)
+from pydantic import BaseModel
+import bentoml
+from bentoml.io import Image, JSON, Multipart, NumpyNdarray
+import time
+import numpy as np
+import boto3
+import os
+from diffusers.utils import pt_to_pil
+import gc
+from diffusers.utils import load_image
+
+
+# torch.autocast(device_type="cpu", enabled=False)
+
+class StableDiffusionRunnable(bentoml.Runnable):
+    SUPPORTED_RESOURCES = ("nvidia.com/gpu", "cpu")
+    SUPPORTS_CPU_MULTI_THREADING = True
+
+    def __init__(self):
+
+        ##### model stable-diffusion-xl-base-0.9
+        self.txt2img_pipe = StableDiffusionXLPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-0.9", torch_dtype=torch.float16, use_safetensors=True, variant="fp16").to("cuda")
+
+        self.txt2img_refiner = StableDiffusionXLImg2ImgPipeline.from_pretrained("stabilityai/stable-diffusion-xl-refiner-0.9", torch_dtype=torch.float32, use_safetensors=True, variant="fp16").to("cuda")
+        
+
+        self.img2img_pipe = StableDiffusionXLImg2ImgPipeline.from_pretrained("stabilityai/stable-diffusion-xl-refiner-0.9", torch_dtype=torch.float16)
+
+
+    @bentoml.Runnable.method(batchable=False, batch_dim=0)
+    def txt2img(self, data):
+        prompt = data["prompt"]
+        guidance_scale = data.get('guidance_scale', 7.5)
+        height = data.get('height', 1024)
+        width = data.get('width', 1024)
+        no_images = data.get('no_images', 2)
+        num_inference_steps = data.get('num_inference_steps', 70)
+        generator = torch.Generator(self.device)
+        generator.manual_seed(data.get('seed'))
+
+        gc.collect()
+        torch.cuda.empty_cache()
+
+        images_output = []
+        images = self.txt2img_pipe(
+                prompt=prompt,
+                guidance_scale=guidance_scale,
+                height=height,
+                width=width,
+                num_inference_steps=num_inference_steps,
+                generator=generator,
+                num_images_per_prompt=no_images
+            ).images
+            
+
+        
+        for image in images:
+            image_refiner = self.txt2img_refiner(prompt=prompt, image=image[None, :]).images[0]
+            images_output.append(image_refiner)
+
+
+        return images_output, prompt
+
+    @bentoml.Runnable.method(batchable=False, batch_dim=0)
+    def img2img(self, data):
+        
+
+        prompt = data["prompt"]
+        strength = data.get('strength', 0.8)
+        guidance_scale = data.get('guidance_scale', 7.5)
+        num_inference_steps = data.get('num_inference_steps', 50)
+        generator = torch.Generator(self.device)
+        generator.manual_seed(data.get('seed'))
+        no_images = data.get('no_images', 2)
+        image_url = data.get('image_url', '')
+
+        gc.collect()
+        torch.cuda.empty_cache()
+
+        
+        init_image = load_image(image_url).convert("RGB")
+
+        images = self.img2img_pipe(
+            prompt=prompt,
+            image=init_image,
+            strength=strength,
+            guidance_scale=guidance_scale,
+            num_inference_steps=num_inference_steps,
+            generator=generator,
+            num_images_per_prompt=no_images
+        ).images
+        
+        return images, prompt
+
+
+stable_diffusion_runner = bentoml.Runner(StableDiffusionRunnable, name='stable_diffusion_runner', max_batch_size=10)
+
+svc = bentoml.Service("stable_diffusion_fp32", runners=[stable_diffusion_runner])
+svc.add_asgi_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"], expose_headers=["*"])
+
+
+def generate_seed_if_needed(seed):
+    if seed is None:
+        generator = torch.Generator()
+        seed = torch.seed()
+    return seed
+
+def upload_images(prompt, images):
+    url = []
+    
+
+    bucket_name = Instance.S3_BUCKET_NAME
+
+
+    # Upload the file
+    s3_client = boto3.client('s3',
+                            aws_access_key_id=Instance.AWS_ACCESS_KEY_ID,
+                            aws_secret_access_key=Instance.AWS_SECRET_ACCESS_KEY)
+    try:
+        for image in images:
+            file_name = f"{sha256(prompt.encode('utf-8')).hexdigest()}_{int(time.time()*1000000)}.jpeg"
+            image.save(file_name)
+
+            response = s3_client.upload_file(file_name, bucket, object_name)
+            url.append(f"{Instance.S3_BUCKET_BASED_END_POINT}/{bucket_name}/{file_name}")
+    except ClientError as e:
+        logging.error(e)
+        return []
+    return url
+
+class Txt2ImgInput(BaseModel):
+    prompt: str
+    guidance_scale: float = 7.5
+    height: int = 512
+    width: int = 512
+    num_inference_steps: int = 50
+    safety_check: bool = True
+    seed: int = None
+    no_images: int = 2
+
+@svc.api(input=JSON(pydantic_model=Txt2ImgInput), output=NumpyNdarray())
+def txt2img(data, context):
+    data = data.dict()
+    data['seed'] = generate_seed_if_needed(data['seed'])
+    images, prompt = stable_diffusion_runner.txt2img.run(data)
+    url = upload_images(prompt,images)
+        
+    for i in data:
+        context.response.headers.append(i, str(data[i]))
+    return NumpyNdarray.from_sample(np.array(url))
+
+class Img2ImgInput(BaseModel):
+    prompt: str
+    strength: float = 0.8
+    guidance_scale: float = 7.5
+    num_inference_steps: int = 50
+    safety_check: bool = True
+    seed: int = None
+    image_url: str
+    no_images: int = 2
+
+
+@svc.api(input=JSON(pydantic_model=Img2ImgInput), output=NumpyNdarray())
+def img2img(data, context):
+    data = data.dict()
+    data['seed'] = generate_seed_if_needed(data['seed'])
+    image, prompt = stable_diffusion_runner.img2img.run(img, data)
+    url = upload_images(prompt,images)
+        
+    for i in data:
+        context.response.headers.append(i, str(data[i]))
+    return NumpyNdarray.from_sample(np.array(url))
+
